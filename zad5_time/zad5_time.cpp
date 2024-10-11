cd16bcf4a2db8529eb417c4ce6ad45899a9ebbb6
Rom-haguko <romagureev170@gmail.com>
2024-10-11 14:51:32 +0300

    Добавьте файлы проекта.

---
+++ zad5_time/zad5_time.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+
+
+using namespace std;
+void main()
+{
+
+
+	setlocale(LC_ALL, "Rus");    //Добавляем русские символы в консоль
+
+
+
+	int hours, min, sec;
+	cout << "Введите часы, минуты, секунды: ";
+	cin >> hours >> min >> sec;
+	min = min + (sec / 30);
+	cout << hours << " часa(ов) " << min << " минут(ы) или ";
+	hours = hours + (min/ 30);
+	cout << hours << " часа(ов) " << endl;
+
+
+
+}
+
+
